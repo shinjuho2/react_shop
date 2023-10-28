c8e0e73278ed42de21c98dff28869512006b5d26
shinjuho2 <sinjuho2@gmail.com>
2023-10-28 09:18:43 +0900

    3일

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 import img from './img/bg.png';
+import { useState } from 'react';
+import data from './data.js';
 
 function App() {
+
+  let [shoes] = useState(data)
+
   return (
     <div className="App">
       <Navbar expand="lg" className="bg-body-tertiary">
@@ -35,26 +40,23 @@ function App() {
 
     <div className='container'>
       <div className='row'>
-        <div className='col-md-4'>
-          <img src='https://codingapple1.github.io/shop/shoes1.jpg' width="80%"/>
-          <h4>상품명</h4>
-          <p>상품설명</p>
-        </div>
-        <div className='col-md-4'>
-          <img src='https://codingapple1.github.io/shop/shoes2.jpg' width="80%"/>
-          <h4>상품명</h4>
-          <p>상품설명</p>
-        </div>
-        <div className='col-md-4'>
-          <img src='https://codingapple1.github.io/shop/shoes3.jpg' width="80%"/>
-          <h4>상품명</h4>
-          <p>상품설명</p>
-        </div>
+        <Product shoes={shoes}></Product>
+        <Product shoes={shoes}></Product>
+        <Product shoes={shoes}></Product>
       </div>
     </div>
-
     </div>
   );
+}
+
+function Product(props){
+  return(
+    <div className='col-md-4'>
+      <img src='https://codingapple1.github.io/shop/shoes1.jpg' width="80%"/>
+      <h4>{props.shoes[2].title}</h4>
+      <p>{props.shoes[2].price}</p>
+    </div>
+  )
 }
 
 export default App;
