9d78f9a7f58b8fc737c4434ba63e62b0d5889880
shinjuho2 <sinjuho2@gmail.com>
2023-11-07 23:41:42 +0900

    11일

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -7,8 +7,11 @@ function Detail(props) {
   let [alert, setalert] = useState(true)
 
   useEffect(() => {
-    let timer = setTimeout(() => { setalert(false) }, 2000);
-  })
+    let timer = setTimeout(() => { setalert(false) }, 2000)
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [])
 
   let [count, setcount] = useState(0);
   let { id } = useParams();
@@ -29,13 +32,14 @@ function Detail(props) {
         setcount(count + 1)
       }}>button</button>
       <div className="row">
-      <input ></input>
         <div className="col-md-6">
           <img src={'https://codingapple1.github.io/shop/shoes' + (id) + '.jpg'} width="100%" />
         </div>
         <div className="col-md-6 mt-4">
           <h4 className="pt-5">{find.title}</h4>
           <p>{find.content}</p>
+          <p>수량</p>
+          <input></input>
           <p>{find.price}원</p>
           <button className="btn btn-danger">주문하기</button>
         </div>
